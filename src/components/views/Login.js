55e87d44d7257efcb66483a7cf8d99a9c9eaeb4f
Dedphish <>
2023-04-05 14:50:13 +0200

    implemented login and register views; no Login/Register functionality yet

--- src/components/views/Login.js
+++ src/components/views/Login.js
@@ -5,7 +5,9 @@ import {useHistory} from 'react-router-dom';
 import {Button} from 'components/ui/Button';
 import 'styles/views/Login.scss';
 import BaseContainer from "components/ui/BaseContainer";
-import PropTypes from "prop-types";
+import {BaseBox} from "../ui/BaseBox";
+import {Input} from "../ui/Input";
+import logo from "../../styles/assets/logo.png";
 
 /*
 It is possible to add multiple components inside a single file,
@@ -13,36 +15,16 @@ however be sure not to clutter your files with an endless amount!
 As a rule of thumb, use one file per component and only add small,
 specific components that belong to the main one in the same file.
  */
-const FormField = props => {
-  return (
-    <div className="login field">
-      <label className="login label">
-        {props.label}
-      </label>
-      <input
-        className="login input"
-        placeholder="enter here.."
-        value={props.value}
-        onChange={e => props.onChange(e.target.value)}
-      />
-    </div>
-  );
-};
-
-FormField.propTypes = {
-  label: PropTypes.string,
-  value: PropTypes.string,
-  onChange: PropTypes.func
-};
 
 const Login = props => {
   const history = useHistory();
-  const [name, setName] = useState(null);
-  const [username, setUsername] = useState(null);
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
   const doLogin = async () => {
+    /*
     try {
-      const requestBody = JSON.stringify({username, name});
+      const requestBody = JSON.stringify({username, password});
       const response = await api.post('/users', requestBody);
 
       // Get the returned user and update a new object.
@@ -52,37 +34,57 @@ const Login = props => {
       localStorage.setItem('token', user.token);
 
       // Login successfully worked --> navigate to the route /game in the GameRouter
-      history.push(`/game`);
+      history.push(`/register`);
     } catch (error) {
       alert(`Something went wrong during the login: \n${handleError(error)}`);
     }
+    */
   };
 
   return (
     <BaseContainer>
-      <div className="login container">
-        <div className="login form">
-          <FormField
-            label="Username"
-            value={username}
-            onChange={un => setUsername(un)}
-          />
-          <FormField
-            label="Name"
-            value={name}
-            onChange={n => setName(n)}
-          />
-          <div className="login button-container">
+      <BaseBox>
+        <div className="login main-container">
+          <div className="login form">
+            <Input
+                placeholder="USERNAME"
+                value={username}
+                onChange={un => setUsername(un)}
+            />
+            <Input
+                type="password"
+                placeholder="PASSWORD"
+                value={password}
+                onChange={pw => setPassword(pw)}
+            />
+            <div className="login form button-container">
+              <Button
+                  disabled={!username || !password}
+                  onClick={() => doLogin()}
+              >
+                Login
+              </Button>
+              <Button
+                  onClick={() => history.push(`/register`)}
+              >
+                Register
+              </Button>
+            </div>
             <Button
-              disabled={!username || !name}
-              width="100%"
-              onClick={() => doLogin()}
+                margin="auto"
+                onClick={() => history.push(`/home`)}
             >
-              Login
+              Play as guest
             </Button>
           </div>
+          <div className="login graphics">
+            <img
+                className="login graphics logo"
+                src={logo} alt="logo"
+            />
+          </div>
         </div>
-      </div>
+      </BaseBox>
     </BaseContainer>
   );
 };
--- src/components/views/Register.js
+++ src/components/views/Register.js
@@ -1,11 +1,91 @@
 import BaseContainer from "../ui/BaseContainer";
+import {Button} from "../ui/Button";
+import {Input} from "../ui/Input";
+import {BaseBox} from "../ui/BaseBox";
+import 'styles/views/Register.scss';
+import logo from 'styles/assets/logo.png'
+import {useState} from "react";
+import {useHistory} from "react-router-dom";
+import {api} from "../../helpers/api";
+import User from "../../models/User";
 
 const Register = () => {
+
+    const history = useHistory();
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [passwordMatch, setPasswordMatch] = useState(false);
+
+    const doRegister = async () => {
+        /*
+        try {
+            const requestBody = JSON.stringify({username, password});
+            const response = await api.post('/users', requestBody);
+
+            // Get the returned user and update a new object.
+            const user = new User(response.data);
+
+            // Store the token into the local storage.
+            localStorage.setItem('token', user.token);
+
+            // Login successfully worked --> navigate to the route /game in the GameRouter
+            history.push(`/home`);
+        }
+        catch (error) {
+            if(error.response.status === 409)
+            {
+                alert(`${error.response.data.message}`)
+            }
+            else{
+                alert(`Something went wrong`);
+            }
+        }
+        */
+    };
+
     return (
         <BaseContainer>
-            <div>
-                <h1>Register</h1>
-            </div>
+            <BaseBox>
+                <div className="register main-container">
+                    <div className="register form">
+                        <Input
+                            placeholder="USERNAME"
+                            value={username}
+                            onChange={un => setUsername(un)}
+                        />
+                        <Input
+                            placeholder="PASSWORD"
+                            type={"password"}
+                            value={password}
+                            onChange={pw => setPassword(pw)}
+                        />
+                        <Input
+                            placeholder="CONFIRM PASSWORD"
+                            type={"password"}
+                            onChange={e => {setPasswordMatch(e === password)}}
+                        />
+                        <div className="register form button-container">
+                            <Button
+                                disabled={!username || !password || !passwordMatch}
+                                onClick={() => console.log(username+password)}
+                            >
+                                Create Account
+                            </Button>
+                            <Button
+                                onClick={() => history.push("/login")}
+                            >
+                                Back
+                            </Button>
+                        </div>
+                    </div>
+                    <div className="register graphics">
+                        <img
+                            className="register graphics logo"
+                            src={logo} alt="logo"
+                        />
+                    </div>
+                </div>
+            </BaseBox>
         </BaseContainer>
     );
 }
