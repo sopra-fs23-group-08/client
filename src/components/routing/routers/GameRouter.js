8e9538df362325efbfdd94c279ad50c26e851a8e
YatingPan <panyating0322@gmail.com>
2023-04-08 18:09:43 +0200

    add a framework for game

--- src/components/routing/routers/GameRouter.js
+++ src/components/routing/routers/GameRouter.js
@@ -1,4 +1,4 @@
-import {Redirect, Route} from "react-router-dom";
+import { Redirect, Route } from "react-router-dom";
 import Game from "components/views/Game";
 import PropTypes from 'prop-types';
 
@@ -7,22 +7,19 @@ const GameRouter = props => {
    * "this.props.base" is "/app" because as been passed as a prop in the parent of GameRouter, i.e., App.js
    */
   return (
-    <div style={{display: 'flex', flexDirection: 'column'}}>
+    <div style={{display: 'flex', flexDirection: 'column', height: '100%'}}>
       <Route exact path={`${props.base}/lobby`}>
-        <Game/>
+        <Game />
       </Route>
       <Route exact path={`${props.base}`}>
-        <Redirect to={`${props.base}/dashboard`}/>
+        <Redirect to={`${props.base}/dashboard`} />
       </Route>
     </div>
   );
 };
-/*
-* Don't forget to export your component!
- */
 
 GameRouter.propTypes = {
   base: PropTypes.string
-}
+};
 
 export default GameRouter;
