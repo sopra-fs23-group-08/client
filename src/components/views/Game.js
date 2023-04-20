6adfcbc51fb1e4190ff44abafc97f2ff6f09b930
YatingPan <panyating0322@gmail.com>
2023-04-20 11:34:22 +0200

    set main components and StompJs and WebSocket of Game

--- src/components/views/Game.js
+++ src/components/views/Game.js
@@ -1,93 +1,367 @@
-import React, { useState, useEffect } from 'react';
-import socketIOClient from 'socket.io-client';
-import EndOfRound from '../ui/EndOfRound';
-import { HowToPlay } from './components/ui/HowToPlay';
+import { useState, useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+import Stomp from 'stompjs';
+import HowToPlay from '../components/ui/HowToPlay';
+import {
+  Grid,
+  Card,
+  CardContent,
+  CardActions,
+  Button,
+  Typography,
+  TextField,
+  IconButton,
+  LinearProgress, // for video but haven't used
+} from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import LeaveIcon from "@material-ui/icons/ExitToApp";
+import HelpIcon from "@material-ui/icons/Help";
+import EndOfGame from "./EndOfGame";
 
+// styles
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  card: {
+    margin: theme.spacing(1),
+    textAlign: "center",
+  },
+  video: {
+    backgroundColor: "red",
+    width: "100%",
+    height: "0",
+    paddingBottom: "56.25%", // 16:9 aspect ratio
+    position: "relative",
+    overflow: "hidden",
+  },
+  thumbnail: {
+    position: "absolute",
+    width: "100%",
+    height: "100%",
+    left: "0",
+    top: "0",
+  },
+  playerButton: {
+    width: "100%",
+    marginBottom: theme.spacing(1),
+  },
+  callButton: {
+    marginTop: theme.spacing(2),
+    width: "100%",
+  },
+  raiseButton: {
+    marginTop: theme.spacing(1),
+    width: "100%",
+  },
+}));
 
-const ENDPOINT = 'http://localhost:4001';
 
 const Game = () => {
-  const [players, setPlayers] = useState([]);
-  const [currentVideo, setCurrentVideo] = useState(null);
-  const [currentRound, setCurrentRound] = useState(0);
-  const [currentPot, setCurrentPot] = useState(0);
-  const [myCards, setMyCards] = useState([]);
-  const [winner, setWinner] = useState(null);
-  const [showHowToPlay, setShowHowToPlay] = useState(false);
+  const location = useLocation();
+  const [stompClient, setStompClient] = useState(null);
+  const [gamePhase, setGamePhase] = useState(null);
+  const [playerList, setPlayerList] = useState([]);
+  const [video, setVideo] = useState({});  //?
+  const [pot, setPot] = useState(0);
+  const [callAmount, setCallAmount] = useState(0);
+  const [myHand, setMyHand] = useState([]);
+  const [decision, setDecision] = useState('fold');  //?
+  const [showHelp, setShowHelp] = useState(false);
+  const [showEndOfRound, setShowEndOfRound] = useState(false);
+  const [showDown, setShowDown] = useState([]);
 
+  const [bigBlind, setBigBlind] = useState(0);
+  const [smallBlind, setSmallBlind] = useState(0);
+
+  const classes = useStyles();
+
+
+  // define state variables for video data
+  const [videoData, setVideoData] = useState({
+    title: "",
+    thumbnail: "",
+    releaseDate: "",
+    likes: "",
+    length: "",
+    views: ""
+  });
+
+
+  // initialize stomp client and subscribe to channels
   useEffect(() => {
-    const socket = socketIOClient(ENDPOINT);
-    socket.on('connect', () => {
-      console.log('Connected to server!');
+    const gameId = location.pathname.split('/')[2];
+    const socket = new WebSocket(`ws://${window.location.host}/game/${gameId}`);
+    const stomp = Stomp.over(socket);
+
+    setStompClient(stomp);
+
+    // set up stomp.subscribe channels
+    stomp.connect({}, () => {
+      
+      // video, subscribe: topic/games/{gameId}/state/video, to get video data
+      stomp.subscribe(`/topic/games/${gameId}/state/video`, (message) => {
+        const data = JSON.parse(message.body);
+        setVideoData({
+          title: data.title,
+          thumbnail: data.thumbnail,
+          releaseDate: data.releaseDate,
+          likes: data.likes,
+          length: data.length,
+          views: data.views
+        });
+      });
+
+      // general, subscribe: topic/games/{gameId}/state/general
+      stomp.subscribe(`/topic/games/${gameId}/state/general`, (message) => {
+        const data = JSON.parse(message.body);
+        setPlayerList(data.players);
+        setBigBlind(data.bigBlind);
+        setSmallBlind(data.smallBlind);
+        setCallAmount(data.callAmount);
+        setPot(data.potAmount);
+        setGamePhase(data.gamePhase);
+      });
+
+     // hand, subscribe: topic/games/{gameId}/players/{userId}/hand, to get comments data
+     stomp.subscribe(`/topic/games/${gameId}/players/${localStorage.getItem('userId')}/hand`, (message) => {
+      const data = JSON.parse(message.body);
+      const comments = data.comments.map(comment => ({
+        content: comment.content,
+        author: comment.author,
+        date: comment.date,
+        likes: comment.likes,
+        is_matched: comment.is_matched,
+      }));
+      setMyHand(comments);
     });
 
-    socket.on('players', (data) => {
-      setPlayers(data.players);
-    });
+      //players, subscribe: topic/games/{gameId}/players, to get all players' data
+      stomp.subscribe(`/topic/games/${gameId}/players`, (message) => {
+        const data = JSON.parse(message.body);
+        const players = data.map(player => {
+          return {
+            username: player.username,
+            decision: player.decision,
+            callAmount: player.callAmount
+          }
+        });
+        setPlayerList(players);
+      });
+      
+      const gameId = location.pathname.split('/')[2];
+      const videoSubscription = stompClient.subscribe(
+        `/games/${gameId}/state/video`,
+        (message) => {
+          const videoData = JSON.parse(message.body);
+          handleVideoUpdate(videoData);
+        }
+      );
+      return () => {
+        videoSubscription.unsubscribe();
+      };
+    }, [stompClient, location.pathname]);
+  
+      // update video
+      const handleVideoUpdate = (message) => {
+        const data = JSON.parse(message.body);
+        setVideoData({
+          title: data.title,
+          thumbnail: data.thumbnail,
+          releaseDate: data.releaseDate,
+          likes: data.likes,
+          length: data.length,
+          views: data.views,
+        });
+      };
+  
+      // update general
+      const handleGameStateUpdate = (message) => {
+        const data = JSON.parse(message.body);
+        setPlayerList(data.players);
+        setPot(data.potAmount);
+        setCallAmount(data.callAmount);
+        setGamePhase(data.gamePhase);
+      };
+      
+      // update players
+      const handlePlayersUpdate = (message) => {
+        const data = JSON.parse(message.body);
+        const playerData = data.reduce((obj, item) => {
+          obj[item.username] = item.lastDecision;
+          return obj;
+        }, {});
+        setPlayerList(playerData);
+      };
+        
+      // update hand
+      const handleHandUpdate = (message) => {
+        const data = JSON.parse(message.body);
+        const commentsData = data.comments.map((comment) => {
+          return {
+            content: comment.content,
+            author: comment.author,
+            date: comment.date,
+            likes: comment.likes,
+            isMatched: comment.isMatched,
+          };
+        });
+        setComments(commentsData);
+      };
+      
+      //decision button: raise, call
+      //send to: games/game_id/players/player_id/decision
+      //and then update to: games/game_id/state/general
+      const handleDecisionSubmit = () => {
+        let decisionValue;
+        if (decision === 'raise') {
+          decisionValue = parseInt(callAmount);
+        } else if (decision === 'call') {
+          decisionValue = callAmount;
+        } else {
+          decisionValue = 0;
+        }
+        const decisionData = {
+          decision: decision,
+          amount: decisionValue,
+        };
+        stompClient.send(`/app/games/${gameId}/players/${localStorage.getItem('userId')}/decision`, {}, JSON.stringify(decisionData));
+      };
+      
+  
+      // leave button
+      // send to: games/game_id/players/player_id/leave ？？？
+      const handleLeaveGame = () => {
+        const gameId = location.pathname.split('/')[2];
+        stompClient.send(`/app/games/${gameId}/players/${localStorage.getItem('userId')}/leave`, {}, '');
+      };
+  
+  
+      // help button, render How to Play
+      const handleHelpButton = () => {
+        setShowHelp(true);
+      };
 
-    socket.on('video', (data) => {
-      setCurrentVideo(data.video);
-    });
+    return () => {
+      if (stomp) {
+        stomp.disconnect();
+      }
+    };
+  }, [location.pathname]);
 
-    socket.on('round', (data) => {
-      setCurrentRound(data.round);
-    });
 
-    socket.on('pot', (data) => {
-      setCurrentPot(data.pot);
-    });
-
-    socket.on('myCards', (data) => {
-      setMyCards(data.cards);
-    });
-
-    socket.on('winner', (data) => {
-      setWinner(data.winner);
-    });
-
-    return () => socket.disconnect();
-  }, []);
-
-  const handleLeaveGame = () => {
-    const socket = socketIOClient(ENDPOINT);
-    socket.emit('leaveGame');
-    socket.disconnect();
-    // Navigate to home page
-  };
-
-  const handleCall = (amount) => {
-    const socket = socketIOClient(ENDPOINT);
-    socket.emit('call', { amount });
-  };
-
-  const handleRaise = (amount) => {
-    const socket = socketIOClient(ENDPOINT);
-    socket.emit('raise', { amount });
-  };
-
-  const handleHelp = () => {
-    // Handle help button logic
-  };
-
-  return (
-    <div style={{ display: 'flex', flexDirection: 'column', height: '100vh' }}>
-      <div style={{ display: 'flex', flexGrow: 1 }}>
-        <PlayerList players={players} />
-        <VideoInfo currentVideo={currentVideo} currentRound={currentRound} currentPot={currentPot} />
-        <DecisionArea onLeaveGame={handleLeaveGame} onCall={handleCall} onRaise={handleRaise} onHelp={handleHelp} />
-        <HowToPlay />
+    return (
+      <div className={classes.root}>
+        <Grid container spacing={1}>
+          {/* Player list */}
+          <Grid item xs={2}>
+            <Card className={classes.card}>
+              <CardContent>
+                {playerList.map((player, index) => (
+                  <Button
+                    key={index}
+                    className={classes.playerButton}
+                    variant="contained"
+                    color="primary"
+                  >
+                    <Typography variant="subtitle1">{player.username}</Typography>
+                    <Typography variant="body2">
+                      {player.decision} {player.callAmount}
+                    </Typography>
+                  </Button>
+                ))}
+              </CardContent>
+            </Card>
+          </Grid>
+    
+          {/* Video */}
+          <Grid item xs={8}>
+            <Card className={classes.card}>
+              <CardContent>
+                <div className={classes.video}>
+                  <img
+                    className={classes.thumbnail}
+                    src={video.thumbnail}
+                    alt="Video thumbnail"
+                  />
+                </div>
+                <Typography variant="h6">{video.title}</Typography>
+                <Typography variant="caption">
+                  {video.releaseDate} &#8226; {video.likes} likes &#8226; {video.views} views
+                </Typography>
+                <div>
+                  <progress value="0" max={video.length}></progress>
+                </div>
+                <Button variant="contained" color="primary" startIcon={<PlayArrowIcon />}>
+                  Play
+                </Button>
+              </CardContent>
+            </Card>
+          </Grid>
+    
+          {/* My hand */}
+          <Grid item xs={12}>
+            <Card className={classes.card}>
+              <CardContent>
+                <Grid container spacing={1}>
+                  {myHand.map((comment, index) => (
+                    <Grid key={index} item xs={3}>
+                      <Card>
+                        <CardContent>
+                          <Typography variant="body2">{comment.content}</Typography>
+                        </CardContent>
+                      </Card>
+                    </Grid>
+                  ))}
+                </Grid>
+              </CardContent>
+            </Card>
+          </Grid>
+    
+          {/* Game info */}
+          <Grid item xs={2}>
+            <Card className={classes.card}>
+              <CardContent>
+                <Typography variant="h6">Round {gamePhase}</Typography>
+                <Typography variant="h5">Pot: {pot}</Typography>
+                <Typography variant="h6">Amount:</Typography>
+                <TextField
+                  variant="outlined"
+                  fullWidth
+                  type="number"
+                  value={callAmount}
+                  onChange={(e) => setCallAmount(e.target.value)}
+                />
+                <Button
+                  className={classes.callButton}
+                  variant="contained"
+                  color="primary"
+                  onClick={handleDecisionSubmit}
+                >
+                  Call
+                </Button>
+                <Button
+                  className={classes.raiseButton}
+                  variant="contained"
+                  color="secondary"
+                  onClick={handleDecisionSubmit}
+                >
+                  Raise
+                </Button>
+              </CardContent>
+              <CardActions>
+                <IconButton onClick={handleLeaveGame}>
+                  <LeaveIcon />
+                </IconButton>
+                <IconButton onClick={handleHelpButton}>
+                  <HelpIcon />
+                </IconButton>
+              </CardActions>
+            </Card>
+          </Grid>
+        </Grid>
       </div>
-      <MyCommentCards myCards={myCards} />
-      {winner && (
-        <div>
-          {winner && <EndOfRound winner={winner} roundNumber={currentRound} />}  
-          {winner.type === 'game' && <p>Game winner: {winner.player.username}</p>}
-        </div>
-      )}
-    </div>
-  );
-};
-
-
+    );
+  };
 
 export default Game;
