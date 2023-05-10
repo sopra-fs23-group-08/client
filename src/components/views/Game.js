37637e34553a48522601e2cab5577630bc89f294
YatingPan <panyating0322@gmail.com>
2023-05-10 13:37:47 +0200

    change the game.js

--- src/components/views/Game.js
+++ src/components/views/Game.js
@@ -1,28 +1,27 @@
 import { useState, useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
-import Stomp from 'stompjs';
 import StompContext from 'components/contexts/StompContext';
-import "styles/views/TestGame.scss";
+import "styles/views/Game.scss";
 import HowToPlay from 'components/ui/HowToPlay';
 import ShowDown from 'components/ui/ShowDown';
 import EndOfGame from 'components/ui/EndOfGame';
-import { connect } from 'net';
 import axios from 'axios';
-import SockJS from 'sockjs-client';
 import { useContext } from 'react';
+import { useHistory } from 'react-router-dom';
+
 
 
 const Game = () => {
   const location = useLocation();
-  const [playerList, setPlayerList] = useState([]);
+
   const [gamePhase, setGamePhase] = useState(''); //?
   const [winner, setWinner] = useState(null); 
   const [gameEnd, setGameEnd] = useState(false);
-  const [video, setVideo] = useState({});  //?
   const [pot, setPot] = useState(0);
+  const [decisionAmount, setDecisionAmount] = useState(0);
   const [callAmount, setCallAmount] = useState(0);
   const [myHand, setMyHand] = useState([]);
-  const [decision, setDecision] = useState('fold');//?
+  const [playersDecision, setPlayersDecision] = useState([]);
   const [showHowToPlay, setShowHowToPlay] = useState(false);
   const [showShowDown, setShowShowDown] = useState(false);
   const [comments, setComments] = useState([]);
@@ -30,7 +29,6 @@ const Game = () => {
 
   const [bigBlind, setBigBlind] = useState(0);
   const [smallBlind, setSmallBlind] = useState(0);
-  const [handleHelpClick, setHandleHelpClick] = useState(false);
   const [handleShowDown, setHandleShowDown] = useState(false);
   const [handleEndOfGame, setHandleEndOfGame] = useState(false);
   const [token, setToken] = useState(localStorage.getItem('token'));
@@ -39,177 +37,160 @@ const Game = () => {
   const { setStompClient } = useContext(StompContext);
   const { connect } = useContext(StompContext);
 
+  const [playerList, setPlayerList] = useState([]);
+  const history = useHistory();
   // define state variables for video data
+  // {"title":null,"thumbnailUrl":null,"views":69823295,"likes":656792,"releaseDate":null,"duration":"PT1M48S"}
   const [videoData, setVideoData] = useState({
     title: "",
-    thumbnail: "",
-    releaseDate: "",
+    thumbnailUrl: "",
+    views: "",
     likes: "",
-    length: "",
-    views: ""
+    releaseDate: "",
+    duration: "",
   });
 
-  
-  // subscribe to player-list updates
-  const [playersSubscription, setPlayersSubscription] = useState(null);
-  // subscribe to video updates
-  const [videoSubscription, setVideoSubscription] = useState(null);
-  // subscribe to comments updates
-  const [commentsSubscription, setCommentsSubscription] = useState(null);
-  // subscribe to game updates
-  const [gameSubscription, setGameSubscription] = useState(null);
-  // subscribe to decision updates
-  const [decisionSubscription, setDecisionSubscription] = useState(null);
-  // subsribe to the winner
-  const [winnerSubscription, setWinnerSubscription] = useState(null);
-  // subscribe to game end
-  const [gameEndSubscription, setGameEndSubscription] = useState(null);
+  const handleCommentsUpdate = (message) => {
+    const data = JSON.parse(message.body);
+    setComments(data);
+  }
 
-
-  const [user, setUser] = useState(localStorage.getItem('user'));
-  const [gameId, setGameId] = useState(localStorage.getItem('gameId'));
-  const [userId, setUserId] = useState(localStorage.getItem('userId'));
-
-  const createStompClient = async () => {
-    const stompClient = await connect();
-    return stompClient;
-}
-
-
-useEffect(() => {
-  const connectSocket = async () => {
-    const client = await createStompClient();
-    setStompClient(client);
+  const handlePlayerListUpdate = (message) => {
+    const data = JSON.parse(message.body);
+    setPlayerList(data);
   };
-  connectSocket();
-}, []);
+  
+  
+  const handleVideoDataUpdate = (message) => {
+    const data = JSON.parse(message.body);
+    setVideoData(data);
+  };
+
+  const handleGameUpdate = (message) => {
+    const data = JSON.parse(message.body);
+      setPot(data.potAmount);
+      setPlayerList(data.players);
+      setBigBlind(data.bigBlind);
+      setSmallBlind(data.smallBlind);
+      setCallAmount(data.callAmount);
+      setGamePhase(data.gamePhase);
+  };
+  
+
+  useEffect(() => {
+    const createStompClient = async () => {
+      const stompClient = await connect();
+      return stompClient;
+    };
+  
+    if (!stompClient) {
+      createStompClient().then((client) => {
+        setStompClient(client);
+      });
+    }
+  }, [stompClient, connect, setStompClient]);
+
 
     useEffect(() => {
-      if (stompClient) {
-        const GameSubscription = stompClient.subscribe(
-          `/topic/games/${gameId}/state`,
-          (message) => {
-              const data = JSON.parse(message.body);
-              setPot(data.potAmount);
-              setPlayerList(data.players);
-              setBigBlind(data.bigBlind);
-              setSmallBlind(data.smallBlind);
-              setCallAmount(data.callAmount);
-              setGamePhase(data.gamePhase);
-          }
+      const gameId = location.pathname.split("/")[2];
+      const token = localStorage.getItem("token");
+    // Subscribe to the game
+    let gameSubscription;
+    if (stompClient) {
+      gameSubscription = stompClient.subscribe(
+        `/topic/games/${gameId}/state`,
+        handleGameUpdate
       );
-        const VideoSubscription  = stompClient.subscribe(
-          `/topic/games/${gameId}/state/video`,
-          (message) => {
-              const data = JSON.parse(message.body);
-              setVideoData({
-                  title: data.title,
-                  thumbnail: data.thumbnail,
-                  releaseDate: data.releaseDate,
-                  likes: data.likes,
-                  length: data.length,
-                  views: data.views
-                });
-              }
-            );
-        const CommentsSubscription = stompClient.subscribe(
-          `/topic/games/${gameId}/players/${token}/hand`,
-          (message) => {
-              const data = JSON.parse(message.body);
-              setComments({
-                  content: data.content,
-                  author: data.author,
-                  date: data.date,
-                  likes: data.likes,
-                  isMatched: data.is_matched
-                });
-              }
-            );
 
+    // Subscribe to the video
+      const videoSubscription = stompClient.subscribe(
+        `/topic/games/${gameId}/video`,
+        handleVideoDataUpdate
+      );
+      const playerListSubscription = stompClient.subscribe(
+        `/topic/games/${gameId}/players`,
+        handlePlayerListUpdate
+      );
+      
+      //subscribe to the comments
+        const commentsSubscription = stompClient.subscribe(
+          `/topic/games/${gameId}/players/${token}/hand`,
+            handleCommentsUpdate
+        );
+        //subscribe to the decision
         const decisionSubscription = stompClient.subscribe(
           `/topic/games/${gameId}/players/${token}/decision`,
           (message) => {
-              const data = JSON.parse(message.body);
-              const player = playerList.find(player => player.username === data.username);
-              const decision = player.lastDecision ? player.lastDecision.action : "No decision";
-              const callAmount = player.lastDecision ? player.lastDecision.amount : "N/A";
-              setDecision({
-                  username: player.username,
-                  decision: player.lastDecision,
-                  callAmount: player.callAmount,
-                  score: player.score,
-                  token: player.token
-                });
-              }
-            );
-        const WinnerSubscription = stompClient.subscribe(
-          `/topic/games/${gameId}/winner`,
+            const data = JSON.parse(message.body);
+            setPlayersDecision(data);
+          }
+        );
+        //subscribe to the winner 
+        const winnerSubscription = stompClient.subscribe(
+          `/topic/games/${gameId}/state/winner`,
           (message) => {
-              const data = JSON.parse(message.body);
-              setWinner({
-                  username: data.username,
-                  score: data.score,
-                  token: data.token
-                });
-              }
-            );
-      const videoSubscription = stompClient.subscribe(
-          `/topic/games/${gameId}/end`,
+            const data = JSON.parse(message.body);
+            setWinner(data);
+          }
+        );
+        //subscribe to the game end
+        const gameEndSubscription = stompClient.subscribe(
+          `/topic/games/${gameId}/state/end`,
           (message) => {
-              const data = JSON.parse(message.body);
-              setGameEnd(true);
-            });
+            const data = JSON.parse(message.body);
+            setGameEnd(data);
+          }
+        );
+        return () => {
+          if (gameSubscription) {
+            gameSubscription.unsubscribe();
+          }
+          if (videoSubscription) {
+            videoSubscription.unsubscribe();
+          }
+          if (playerListSubscription) {
+            playerListSubscription.unsubscribe();
+          }
+          if (commentsSubscription) {
+            commentsSubscription.unsubscribe();
+          }
+          if (decisionSubscription) {
+            decisionSubscription.unsubscribe();
+          }
+          if (winnerSubscription) {
+            winnerSubscription.unsubscribe();
+          }
+          if (gameEndSubscription) {
+            gameEndSubscription.unsubscribe();
+          };
+        };
       }
-    // CLEANUP and Unscribe //
-    return () => {
-      if (stompClient && stompClient.connected) {
-        stompClient.disconnect();
-      }
-      if (gameSubscription) {
-        gameSubscription.unsubscribe();
-      }
-      if (videoSubscription) {
-        videoSubscription.unsubscribe();
-      }
-      if (commentsSubscription) {
-        commentsSubscription.unsubscribe();
-      }
-      if (decisionSubscription) {
-        decisionSubscription.unsubscribe();
-      }
-      if (winnerSubscription) {
-        winnerSubscription.unsubscribe();
-      }
-      if (gameEndSubscription) {
-        gameEndSubscription.unsubscribe();
-      }
-    };
-  }, [stompClient, gameId, token, playerList, gamePhase, callAmount, pot, bigBlind, smallBlind, videoData, comments, decision, winner, gameEnd]);
+    }, [stompClient, location.pathname]);
 
-  const handleDecisionSubmit = () => {
-    const gameId = location.pathname.split('/')[2];
-    let decisionValue;
-    if (decision === 'raise') {
-      decisionValue = parseInt(callAmount);
-    } else if (decision === 'call') {
-      decisionValue = parseInt(callAmount);
-    } else {
-      decisionValue = 0;
-    }
-    const decisionData = {
-      decision: decision,
-      amount: decisionValue,
-    };
-    stompClient.send(`/app/games/${gameId}/players/${localStorage.getItem('token')}/decision`, {}, JSON.stringify(decisionData));
-  };
 
+
+    const handleDecisionSubmit = (decisionType) => {
+      const gameId = location.pathname.split('/')[2];
+      const token = localStorage.getItem('token');
+      const decisionValue = parseInt(decisionAmount) || 0;
+      const decisionData = {
+        token: token,
+        decision: decisionType,
+        amount: decisionValue,
+      };
+      stompClient.send(`/app/games/${gameId}/players/${token}/decision`, {}, JSON.stringify(decisionData));
+    };
+    
+    const handleCall = () => {
+      handleDecisionSubmit('call');
+    };
+    
+    const handleRaise = () => {
+      handleDecisionSubmit('raise');
+    };
+    
     const handleLeaveGame = () => {
       setShowLeaveModal(true);
-      const gameId = location.pathname.split('/')[2];
-      const { username, token } = playerList.find(player => player.token === localStorage.getItem('token'));
-      stompClient.send(`/app/games/${gameId}/players/remove`, {}, JSON.stringify({ username: username, token: token }));
-      // Update player list in front-end
-      setPlayerList((prevPlayerList) => prevPlayerList.filter((player) => player.username !== username));
     };
 
     // confirm leave
@@ -219,18 +200,22 @@ useEffect(() => {
       const token = localStorage.getItem('token');
       stompClient.send(`/app/games/${gameId}/players/remove`, {}, JSON.stringify({ username: username, token: token }));
       // Update player list in front-end
-      setPlayerList((prevPlayerList) => prevPlayerList.filter((player) => player.username !== username));
+      handlePlayerListUpdate((prevPlayerList) => prevPlayerList.filter((player) => player.username !== username));
       setShowLeaveModal(false);
       // redirect to home page
-      window.location.href = '/home';
+      console.log('history:', history);
+      history.push('/home');
     };
     // cancel leave
     const handleCancelLeave = () => {
       setShowLeaveModal(false);
     };
 
-  
-  useEffect(() => {
+    const handleHelpClick = () => {
+      setShowHowToPlay(true);
+    };
+
+    // handle winner
     const handleGameEnd = (gameEndMessage) => {
       setWinner(gameEndMessage.winner);
       setGamePhase(gameEndMessage.gamePhase);
@@ -244,39 +229,43 @@ useEffect(() => {
     const handleCloseShowDown = () => {
       setShowShowDown(false);
     };
-  }, [location.pathname]);
+
 
   return (
     <div className = "game">
       <div className="box">    
-        <div className="left">
-          <ul className="player-list">
-            {playerList.map((player, index) => (
-            <li key={index}>
-              <a href={player.token ? `/users/${player.username}` : null}>{player.username}</a>
-              {player.lastDecision && (
-                <span>
-                  {player.lastDecision.type === 'call'
-                    ? `Called ${player.lastDecision.amount} points`
-                    : player.lastDecision.type === 'raise'
-                    ? `Raised ${player.lastDecision.amount} points`
-                    : 'Folded'}
-                    , has {player.score} points
-                </span>
-              )}
-            </li>
-          ))}
-        </ul>
+      <div className="left">
+      <ul className="player-list">
+  {playerList?.length > 0 && playerList.map((player, index) => (
+    <li key={index}>
+      <a href={player.token ? `/users/${player.username}` : null}>{player.username}</a>
+      <div>Score: {player.score}</div>
+      {player.lastDecision && (
+        <div>
+          Last decision: {player.lastDecision === "NOT_DECIDED"
+            ? "Not decided"
+            : player.lastDecision.type === "call"
+            ? `Called ${player.lastDecision.amount} points`
+            : player.lastDecision.type === "raise"
+            ? `Raised ${player.lastDecision.amount} points`
+            : "Folded"
+          }
+        </div>
+)}
+    </li>
+  ))}
+</ul>
       </div>
+
 
   
         <div className="center">
           <div className="title">{videoData.title}</div>
           <div className="video-container">
-            <div className="thumbnail" style={{ backgroundImage: `url(${videoData.thumbnail})` }}></div>
+            <div className="thumbnail" style={{ backgroundImage: `url(${videoData.thumbnailUrl})` }}></div>
           </div>
           <div className="stats">
-            <p>Date: {videoData.releaseDate}</p>
+            <p>Release Date: {videoData.releaseDate}</p>
             <p>Likes: {videoData.likes}</p>
             <p>Views: {videoData.views}</p>
           </div>
@@ -287,54 +276,57 @@ useEffect(() => {
       <div className="right">
         <div className="round-number">Round {gamePhase}</div>
         <div className="pot-amount">pot: {pot}</div>
-        <div className="input-amount">
-          <input className="input-amount" type="number" placeholder="Enter amount..." />
-        </div>
+        <input
+          className="input-amount"
+          type="number"
+          placeholder="Enter amount..."
+          value={decisionAmount}
+          onChange={(event) => setDecisionAmount(event.target.value)}
+        />
         <div className="button-container">
-          <button className="call-button" onClick={handleDecisionSubmit}>call</button>
-          <button className="raise-button" onClick={handleDecisionSubmit}>raise</button>
+          <button className="call-button" onClick={handleCall}>call</button>
+          <button className="raise-button" onClick={handleRaise}>raise</button>
           <button className="left-button" onClick={handleLeaveGame}>leave</button>
+            <dialog open={ShowLeaveModal}>
+              <div className="modal">
+                <p>Are you sure you want to leave the game?</p>
+                <div className="button-container">
+                  <button className="confirm-button" onClick={handleConfirmLeave}>Yes</button>
+                  <button className="cancel-button" onClick={handleCancelLeave}>No</button>
+                </div>
+              </div>
+            </dialog>
           <button className="help-button" onClick={handleHelpClick}>help</button>
         </div>
       </div>
   
-        <div className="footer">
-          <div className="card-container">
-            {myHand.map((comment, index) => (
-              <div className="card" key={index}>
-                <div className="card-top">
-                  <span>{comment.author}</span>
-                  <i className="fas fa-heart"></i>
-                </div>
-                <div className="card-main">
-                  {comment.content}
-                </div>
-              </div>
-            ))}
+      <div className="footer">
+      <div className="card-container">
+        {comments.map((comment, index) => (
+          <div className="card" key={index}>
+            <div className="card-top">
+              <span>{comment.first.author}</span>
+              <i className="fas fa-heart"></i>
+            </div>
+            <div className="card-main">
+              {comment.first.content}
+            </div>
           </div>
+        ))}
       </div>
+    </div>
             
         {/* end of game section */}
         {gameEnd && <EndOfGame winner={winner} gamePhase={gamePhase} />}
-
-        {/* Show confirmation window when "leave" button is clicked */}
-        { ShowLeaveModal && (
-        <div>
-          <p>Are you sure you want to leave the game?</p>
-          <button onClick={handleLeaveGame}>Yes</button>
-          <button onClick={() => setShowLeaveModal(false)}>No</button>
-        </div>
-        )}
         
-        {/* Show how to play window when "help" button is clicked */}
-        {showHowToPlay && <HowToPlay handleClose={() => setShowHowToPlay(false)} />}
-
+    {/* Show how to play window when "help" button is clicked */}
+    {showHowToPlay && <HowToPlay handleClose={() => setShowHowToPlay(false)} />}
+    
+    {/* Show show down window when "show down" button is clicked */}
+    {showShowDown && <ShowDown handleClose={handleCloseShowDown} />}
     </div>
     </div>
-);
+  );
 };
 
 export default Game;
-  
-  
-  
